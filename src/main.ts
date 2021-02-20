748f8f061332e206ee50633e6bc0bcff70b98db0
Hakan Yalitekin <hyalitekin@tekzen.com.tr>
2021-02-20 14:08:41 +0300

    First upload.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+
+//AppModule burada...
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +10,6 @@ if (environment.production) {
   enableProdMode();
 }
 
+//Başlangıç aşamasında tarıyıcı ile çalış ve AppModule ile çalışmaya başla demek.
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
